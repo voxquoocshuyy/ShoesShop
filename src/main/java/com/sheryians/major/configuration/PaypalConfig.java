80244cda2498848d5996a966a1dee1862c9187e0
voxquoocshuyy <66914322+voxquoocshuyy@users.noreply.github.com>
2021-09-21 09:14:54 +0700

    Update UI

---
+++ src/main/java/com/sheryians/major/configuration/PaypalConfig.java
@@ -0,0 +1,37 @@
+//package com.sheryians.major.configuration;
+//
+//import com.paypal.base.rest.APIContext;
+//import com.paypal.base.rest.OAuthTokenCredential;
+//import com.paypal.base.rest.PayPalRESTException;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class PaypalConfig {
+//    @Value("${paypal.client.id}")
+//    private String clientID;
+//    @Value("${paypal.client.secret}")
+//    private String clientSecret;
+//    @Value("${paypal.mode}")
+//    private String mode;
+//    @Bean
+//    public Map<String, String> paypalSdkConfig(){
+//        Map<String, String> configMap = new HashMap<>();
+//        configMap.put("mode", mode);
+//        return configMap;
+//    }
+//    @Bean
+//    public OAuthTokenCredential oAuthTokenCredential(){
+//        return new OAuthTokenCredential(clientID, clientSecret, paypalSdkConfig());
+//    }
+//    @Bean
+//    public APIContext apiContext() throws PayPalRESTException {
+//        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+//        context.setConfigurationMap(paypalSdkConfig());
+//        return apiContext();
+//    }
+//}
